fa073695375b45f07ca0f94bdf25f70a34602bc7
fffw <fffw@getlantern.org>
2015-08-25 22:39:30 +0800

    fix measured test

--- measured_test.go
+++ measured_test.go
@@ -28,7 +28,7 @@ func TestReportError(t *testing.T) {
 	d := Dialer(net.Dial, "localhost:9000")
 	_, _ = d("tcp", "localhost:9999")
 	_, _ = d("tcp", "localhost:9998")
-	time.Sleep(50 * time.Millisecond)
+	time.Sleep(100 * time.Millisecond)
 	if assert.Equal(t, 2, len(nr.s)) {
 		assert.Equal(t, "errors", nr.s[0].Type, "should report correct server")
 		assert.Equal(t, "localhost:9000", nr.s[0].Tags["server"], "should report correct server")
@@ -50,7 +50,7 @@ func TestDefaultTags(t *testing.T) {
 	defer Stop()
 	runtime.Gosched()
 	reportError("test-server", fmt.Errorf("test-error"), "dial-phase")
-	time.Sleep(50 * time.Millisecond)
+	time.Sleep(100 * time.Millisecond)
 	if assert.Equal(t, 1, len(nr.s)) {
 		assert.Equal(t, "test-app", nr.s[0].Tags["app"], "should report default tags")
 	}
