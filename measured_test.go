448b99b197047cd882f7a7abd9308bfe741c65ab
fffw <fffw@getlantern.org>
2016-04-18 19:19:32 +0800

    measured works in its own scope

--- measured_test.go
+++ measured_test.go
@@ -4,34 +4,13 @@ import (
 	"net"
 	"net/http"
 	"runtime"
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/getlantern/testify/assert"
 )
-
-type mockReporter struct {
-	error   map[Error]int
-	latency []*LatencyTracker
-	traffic []*TrafficTracker
-}
-
-func (nr *mockReporter) ReportError(e map[*Error]int) error {
-	for k, v := range e {
-		nr.error[*k] = v
-	}
-	return nil
-}
-
-func (nr *mockReporter) ReportLatency(l []*LatencyTracker) error {
-	nr.latency = append(nr.latency, l...)
-	return nil
-}
-
-func (nr *mockReporter) ReportTraffic(t []*TrafficTracker) error {
-	nr.traffic = append(nr.traffic, t...)
-	return nil
-}
 
 func TestReportError(t *testing.T) {
 	nr := startWithMockReporter()
@@ -41,6 +20,8 @@ func TestReportError(t *testing.T) {
 	_, _ = d("tcp", "localhost:9998")
 	runtime.Gosched()
 	time.Sleep(100 * time.Millisecond)
+	nr.muReport.Lock()
+	defer nr.muReport.Unlock()
 	if assert.Equal(t, 2, len(nr.error)) {
 		assert.Equal(t, 1, nr.error[Error{"localhost:9999", "connection refused", "dial"}])
 		assert.Equal(t, 1, nr.error[Error{"localhost:9998", "connection refused", "dial"}])
@@ -64,10 +45,8 @@ func TestReportStats(t *testing.T) {
 				if s == http.StateIdle {
 					RemoteAddr = c.RemoteAddr().String()
 					mc := c.(*Conn)
-					bytesIn = mc.BytesIn
-					bytesOut = mc.BytesOut
-					time.Sleep(100 * time.Millisecond)
-					_ = mc.Close()
+					atomic.StoreUint64(&bytesIn, mc.BytesIn)
+					atomic.StoreUint64(&bytesOut, mc.BytesOut)
 				}
 			},
 		}
@@ -85,12 +64,14 @@ func TestReportStats(t *testing.T) {
 	resp, _ := c.Do(req)
 	assert.Equal(t, 404, resp.StatusCode)
 	_ = resp.Body.Close()
-	assert.True(t, bytesIn > 0, "should count bytesIn")
-	assert.True(t, bytesOut > 0, "should count bytesOut")
+	assert.True(t, atomic.LoadUint64(&bytesIn) > 0, "should count bytesIn")
+	assert.True(t, atomic.LoadUint64(&bytesOut) > 0, "should count bytesOut")
 
 	time.Sleep(300 * time.Millisecond)
 	// verify both client and server stats
-	if assert.Equal(t, 3, len(nr.traffic)) {
+	nr.muReport.Lock()
+	defer nr.muReport.Unlock()
+	if assert.Equal(t, 2, len(nr.traffic)) {
 		e := nr.traffic[1]
 		assert.Equal(t, RemoteAddr, e.ID, "should report server stats with Remote addr")
 		assert.Equal(t, bytesIn, e.TotalIn, "should report server stats with bytes in")
@@ -103,10 +84,10 @@ func TestReportStats(t *testing.T) {
 		assert.Equal(t, bytesIn, e.MinOut, "should report same byte count as server")
 		assert.Equal(t, bytesOut, e.MinIn, "should report same byte count as server")
 
-		e = nr.traffic[2]
+		/*e = nr.traffic[2]
 		assert.Equal(t, l.Addr().String(), e.ID, "should report server as Remote addr")
 		assert.Equal(t, uint64(0), e.MinOut, "should only report increased byte count")
-		assert.Equal(t, uint64(0), e.MinIn, "should only report increased byte count")
+		assert.Equal(t, uint64(0), e.MinIn, "should only report increased byte count")*/
 	}
 }
 
@@ -118,4 +99,34 @@ func startWithMockReporter() *mockReporter {
 	// To make sure it really started
 	runtime.Gosched()
 	return &nr
+}
+
+type mockReporter struct {
+	muReport sync.Mutex
+	error    map[Error]int
+	latency  []*LatencyTracker
+	traffic  []*TrafficTracker
+}
+
+func (nr *mockReporter) ReportError(e map[*Error]int) error {
+	nr.muReport.Lock()
+	defer nr.muReport.Unlock()
+	for k, v := range e {
+		nr.error[*k] = v
+	}
+	return nil
+}
+
+func (nr *mockReporter) ReportLatency(l []*LatencyTracker) error {
+	nr.muReport.Lock()
+	defer nr.muReport.Unlock()
+	nr.latency = append(nr.latency, l...)
+	return nil
+}
+
+func (nr *mockReporter) ReportTraffic(t []*TrafficTracker) error {
+	nr.muReport.Lock()
+	defer nr.muReport.Unlock()
+	nr.traffic = append(nr.traffic, t...)
+	return nil
 }
